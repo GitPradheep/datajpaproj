285bfe70bd96c9803669fb4152841c689245fcba
Yamini Durga Pradeep Doddi <yamini.durga.pradeep.doddi@gds.ey.com>
2022-10-01 10:03:56 +0530

    Initial commit

---
+++ src/main/java/com/projectuser/datajpapro/DatajpaproApplication.java
@@ -0,0 +1,13 @@
+package com.projectuser.datajpapro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatajpaproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatajpaproApplication.class, args);
+	}
+
+}
